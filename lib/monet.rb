8f4098a3bac7bf2a02c52410fd6f79a8bb9edfa4
Daniel Angel-Bradford <locusdelicti@gmail.com>
2015-08-25 20:35:02 +1000

    reorganising structure :rage1:

--- lib/monet.rb
+++ lib/monet.rb
@@ -4,8 +4,8 @@ require 'monet/config'
 
 if defined? Rails
   require 'monet/engine'
-  require 'monet/reference_parser'
-  require 'monet/scheme_parser'
+  require 'monet/parsers/reference_parser'
+  require 'monet/parsers/scheme_parser'
   require 'monet/railtie'
   require 'monet/helpers'
 end
